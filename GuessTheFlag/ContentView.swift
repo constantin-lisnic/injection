3be35f65036300a896eccd160ff4d6f27f6a9429
Constantin Lisnic <constantin@lisnic.dev>
2024-11-12 17:10:28 +0000

    feat: package impl

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -5,17 +5,22 @@
 //  Created by Constantin Lisnic on 09/11/2024.
 //
 
+import Inject
 import SwiftUI
 
 struct ContentView: View {
+    @ObserveInjection var inject
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("it's a beautiful day, isn't it?")
+            Text("I never thought I'd make it this far")
         }
         .padding()
+        .enableInjection()
     }
 }
 
